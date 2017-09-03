27b27b149f508e4cf815a91ab4904f5cf5038605
Sasha Tlr <sashatailor@gmail.com>
2017-09-03 18:28:55 -0400

    Ruby algo to compress string

---
+++ ArraysStrings/1_6.rb
@@ -0,0 +1,24 @@
+#String Compression
+#basic string compression using counts of repeated characters
+#eg aabcccccaaa is a2b1c5a3
+
+def stringCompress(fullString)
+
+  result = []
+
+  fullString.each_char do |c|
+
+    if result[-2] != c
+      result << c << 1
+    else
+      result[-1] += 1
+    end
+  end
+
+  if result.length < fullString.length
+    result.join("")
+  else
+    fullString
+  end
+
+end
