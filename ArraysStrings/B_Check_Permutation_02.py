7fae18e3c712a5377c2fc2c51397302ceb0bf018
Sasha Tlr <sashatailor@gmail.com>
2019-02-18 20:56:00 -0500

    check string permutations

---
+++ ArraysStrings/B_Check_Permutation_02.py
@@ -0,0 +1,26 @@
+def check_permutation(string1, string2):
+	if len(string1) != len(string2):
+		return False
+
+	counter = Count()
+
+	for letter in string1:
+		counter[letter] += 1
+
+	for letter in string2:
+		if counter[letter] == 0:
+			return False
+		counter[letter] -= 1
+
+	return True
+
+
+
+class Count(dict):
+	def __missing__(self, key):
+		return 0
+
+
+if __name__ == "__main__":
+	import sys
+	print(check_permutation(sys.argv[-1], sys.argv[-2]))
