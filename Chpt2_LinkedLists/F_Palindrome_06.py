d3a330a44649e89c29fd06aad62cf7111f412028
Sasha Tlr <sashatailor@gmail.com>
2019-03-02 17:08:31 -0500

    Add palindrome recursive solution

--- Chpt2_LinkedLists/F_Palindrome_06.py
+++ Chpt2_LinkedLists/F_Palindrome_06.py
@@ -1,8 +1,11 @@
 from LinkedList import LinkedList
+import unittest
 
-class Result(node, isPal):
-	self.node = node
-	self.isPal = isPal
+class Result():
+
+	def __init__(self, node, isPal):
+		self.node = node
+		self.isPal = isPal
 
 
 def get_list_size(node):
@@ -15,29 +18,45 @@ def get_list_size(node):
 	return size
 
 def isPalindrome(ll):
-	length = get_list_size(ll)
-
-	if length <= 1:
-		return True
-
-	res = isPalindromeRecursive(ll.head, length)
-
+	length = get_list_size(ll.head)
+	res = IsPalindromeRecursive(ll.head, length)
 	return res.isPal
 
 def IsPalindromeRecursive(head, length):
 	if length <= 0 or head is None:
 		return Result(head, True)
-
-	if length == 1:
+	elif length == 1:
 		return Result(head.next, True)
 
 	res = IsPalindromeRecursive(head.next, length - 2)
 
-	if res.Result not True or res.node is None:
+	if res.isPal is False or res.node is None:
 		return res
 
 	res.isPal = res.node.value == head.value
 	res.node = res.node.next
 
 	return res;
+
+class Test(unittest.TestCase):
+	
+	test_data = ([LinkedList().addValues(['a','b', 'b', 'a']), True], [LinkedList().addValues(['a','b', 'a']), True], 
+		[LinkedList().addValues(['a', 'b', 'c', 'a']), False])
+
+	def test_IsPalindrome(self):
+		for [data, expected] in self.test_data:
+			actual = isPalindrome(data)
+			print(actual)
+			self.assertEqual(expected, actual)
+
+
+if __name__ == "__main__":
+	unittest.main()
+
+
+
+
+
+
+
 
