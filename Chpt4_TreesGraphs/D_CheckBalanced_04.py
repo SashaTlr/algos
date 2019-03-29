3df6b1c3c4d8c9c792f94761cc8b338a51f81cf0
Sasha Tlr <sashatailor@gmail.com>
2019-03-28 23:17:45 -0400

    add paths with sums

--- Chpt4_TreesGraphs/D_CheckBalanced_04.py
+++ Chpt4_TreesGraphs/D_CheckBalanced_04.py
@@ -7,20 +7,20 @@ from Graph2 import BTNode
 
 def getHeight(node):
 	if not node:
-		return 0
-
-	if node.left is None and node.right is None:
-		return 1
+		return -1
 
 	heightLeft = getHeight(node.left)
-	heightRight = getHeight(node.left)
+	heightRight = getHeight(node.right)
 
-	if heightLeft - heightRight > 1:
+	if heightLeft < 0 || heightRight < 0:
+		return float('-inf')
+
+	if abs(heightLeft - heightRight) > 1:
 		return float('-inf')
 
 	return max(heightLeft, heightRight) + 1
 
 def isBalanced(node):
 	height = getHeight(node)
-	return height and height > 0:
+	return height > 0:
 
--- Chpt4_TreesGraphs/L_PathsWithSums_12.py
+++ Chpt4_TreesGraphs/L_PathsWithSums_12.py
@@ -1,1 +1,10 @@
-L_PathsWithSums_12.py
+#pathswithsum
+
+#depth first search
+#create hash for every node step
+class Node:
+	def __init__(self, data, left = None, right = None):
+		self.data = data
+
+
+def PathsWithSums
